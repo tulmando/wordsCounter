a6813696347fa6e14f01d3bceb1b0e3c641de44b
Ido Tulman <ido.tulman@ibm.com>
2020-05-03 08:23:38 +0300

    BL simple text

--- bl/wordsBl.js
+++ bl/wordsBl.js
@@ -1,4 +1,6 @@
 // business logic for the words
+const axios = require('axios');
+// TODO helpers
 
 class WordsBl {
     constructor() {
@@ -9,9 +11,27 @@ class WordsBl {
         return await this.wordsCounter.getWordStatistics(word);
     }
 
+    async normalizeWord(word) {
+        // remove all characters which are not letters.
+        // return all letters as lower case.
+        return word.replace(/[^a-zA-Z ]+/g, '').toLowerCase();
+    }
+
     async countWordsFromTextInBody(text) {
-        // TODO implement
-        return
+        let wordDict = {}
+        // remove all white spaces from the text.
+        // keep a space between words (for \n for example).
+        text = text.replace(/\s/g, ' ');
+        let splittedWords = text.split(' ');
+        for (let word of splittedWords) {
+            word = await this.normalizeWord(word);
+            if (word.length === 0) return;
+            wordDict[word] = wordDict[word] || 0;
+            wordDict[word] += 1;
+        }
+
+        console.log('!!!! wordDict: ' + JSON.stringify(wordDict)); // TODO
+        await this.wordsCounter.bulkUpdate(wordDict);
     }
 
     async countWordsFromTextWithStream(source) {
